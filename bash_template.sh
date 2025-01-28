ab21a8e02e49b5e3c7bca8b7fc787e47d5895767
heidithomasma <heidi.e.thomasma@wmich.edu>
2025-01-28 18:02:06 -0500

    learning_bash.sh

---
+++ bash_template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+greeting="Hello"
+your_name=“Heidi”
+echo "$greeting $Heidi!”
+echo "This script is helping you learn bash."
+mkdir -p ~/Desktop/bioinformatics/bash_scripts
+mv ~/Desktop/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/
+mv ~/Desktop/bioinformatics/bash_scripts/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/bash_template.sh
